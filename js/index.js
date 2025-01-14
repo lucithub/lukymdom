bfcf860cc094f5998dfa1e419121f734c4c91628
lucithub <lucasvelez2013@gmail.com>
2025-01-14 18:09:48 -0300

    add randomizer button]

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-// COLORES
+
 let colores = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#FFA500", "#800080", "#008000", "#000080", "#FFC0CB", "#A52A2A", "#FFD700", "#808080", "#FFFFFF", "#000000"];
+let cuantasOpciones = 0;
 /*
 // Usando GSAP para animar entre secciones
 gsap.registerPlugin(ScrollTrigger);
@@ -20,8 +21,13 @@ gsap.utils.toArray(".page").forEach((page, i) => {
 */
 function addOption(){
   let option = document.getElementById("thing-box").value;
+  if (option === "") {
+    alert("Por favor, escriba algo en el cuadro de texto.");
+    return;
+  }
   // Crear un nuevo div para el "cuadrito"
   let newDiv = document.createElement("div");
+  newDiv.id = "options";
     
   // Añadir el texto al div
   newDiv.textContent = option;
@@ -29,13 +35,22 @@ function addOption(){
   // Dar estilo al div (opcional)
   newDiv.style.padding = "10px";
   newDiv.style.margin = "5px";
-  newDiv.style.backgroundColor = "#f0f0f0";
   newDiv.style.borderRadius = "5px";
   newDiv.style.backgroundColor = colores[Math.floor(Math.random() * colores.length)];
   
   // Añadir el div al documento
   document.getElementById("add-thing").after(newDiv);
-  
+  cuantasOpciones++;
   // Limpiar el input
   document.getElementById("thing-box").value = "";
+  if(cuantasOpciones === 1){
+    let startOption = document.createElement("button");
+    startOption.textContent = "Randomizar";
+    startOption.style.padding = "10px";
+    startOption.style.margin = "5px";
+    startOption.style.backgroundColor = "green";
+    startOption.style.borderRadius = "5px";
+    startOption.style.fontFamily = "Baloo 2"
+    document.getElementById("options").after(startOption);
+  }
 }
